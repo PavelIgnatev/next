d0b9a6b76ef4d90398cb35c0109ecdfa6dac277b
Pavel <palllkaignatev@gmail.com>
2023-11-14 03:10:05 +0400

    ssa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -11,6 +12,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
+          <title>AiSender</title>
         </Head>
         <body>
           <Main />
